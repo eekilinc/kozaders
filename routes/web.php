d52881c48ff1abdc20c629f04baefad84b855c54
eekilinc <ekilinc@mehmetakif.edu.tr>
2026-04-19 14:25:04 +0300

    Add authentication and profile management components
    - Created various Blade components for authentication and profile management, including buttons, input fields, and dropdowns.
    - Implemented modal for account deletion confirmation.
    - Developed layout files for app and guest views.
    - Established navigation structure with responsive design.
    - Added routes for authentication, including registration, login, password reset, and email verification.
    - Implemented tests for authentication, email verification, password confirmation, and profile management functionalities.

--- routes/web.php
+++ routes/web.php
@@ -1,33 +1,21 @@
 <?php
 
-use App\Http\Controllers\TestController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\AdminController;
 
-
-Route::get("/",function(){
-    return "burası ana sayfa";
+Route::get('/', function () {
+    return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-
-Route::prefix("users")->group(function(){
- Route::get('/create',[UserController::class,"create"]);
- Route::get('/edit',[UserController::class,"edit"]);
- Route::get('/list',[UserController::class,"list"]);
- Route::get("/test",[UserController::class,"test"]);
-
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::prefix("admin")->group(function(){
-    Route::get("/",[AdminController::class,"index"]);
-
-});
-
-
-
-
-
-
-
+require __DIR__.'/auth.php';
+require __DIR__.'/ozel.php';
